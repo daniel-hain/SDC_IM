ebd004b11ed68f5c9a4375dac977a355e5e5bdbf
Daniel S. Hain <38432063+daniel-hain@users.noreply.github.com>
2023-06-01 17:27:08 +0200

    update

--- notebooks/XX_export.R
+++ notebooks/XX_export.R
@@ -1,6 +1,13 @@
-
-library(tidyverse)
+library(tidyverse) # General DS toolkit
+library(magrittr) # For advanced piping
 library(arrow)
+
+# Databases
+library(DBI) # GEneral R database interface
+library(RPostgres) # PostgreSQL interface driver 
+library(dbplyr) # for dplyr with databases
+
+####### Export Parquet
 
 data <- readRDS('data/CN_el_cpc.rds')
 data %>% write_parquet('data/CN_el_cpc.parquet')
@@ -8,5 +15,17 @@ data %>% write_parquet('data/CN_el_cpc.parquet')
 rm(data)
 
 data <- readRDS('data/CN_el_patent_abstract.rds')
-data %>% write_parquet('CN_el_patent_abstract.parquet')
+data %>% write_parquet('data/CN_el_patent_abstract.parquet')
+rm(data)
+
+data <- readRDS('data/CN_inventor.rds')
+data %>% write_parquet('data/CN_inventor.parquet')
+rm(data)
+
+data <- readRDS('data/CN_el_applt.rds')
+data %>% write_parquet('data/CN_el_applt.parquet')
+rm(data)
+
+data <- readRDS('data/CN_applt.rds')
+data %>% write_parquet('data/CN_applt.parquet')
 rm(data)
